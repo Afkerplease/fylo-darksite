2597a2570baa1edb7cbab01be6347826ec2a0e19
Moscow <afker2020@gmail.com>
2022-11-29 16:29:09 +0100

    3rd section over

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import acces from "./images/icon-access-anywhere.svg";
 import security from "./images/icon-security.svg";
 import collab from "./images/icon-collaboration.svg";
 import store from "./images/icon-any-file.svg";
+import productivity from "./images/illustration-stay-productive.png";
+import arrow from "./images/icon-arrow.svg";
 
 function App() {
   return (
@@ -23,7 +25,7 @@ function App() {
           <h1 className=" absolute top-[55%] text-White text-3xl text-center w-[80%] font-[700]  ">
             All your files in one secure location, accessible anywhere.
           </h1>
-          <p className=" mt-20 text-center text-White w-[67%] ">
+          <p className=" text-[18px]  mt-20 text-center text-White w-[80%] ">
             Fylo stores all your most important files in one secure location.
             Access them wherever you need, share and collaborate with friends
             family, and co-workers.
@@ -38,16 +40,20 @@ function App() {
         <div className=" flex flex-col gap-20  ">
           <div className=" flex flex-col font-Open gap-4 items-center">
             <img src={acces} alt="" />
-            <h2 className=" text-xl text-White">Access your files, anywhere</h2>
-            <p className=" text-center w-[75%] text-White ">
+            <h2 className=" text-2xl font-[700] text-White">
+              Access your files, anywhere
+            </h2>
+            <p className="  text-[18px] text-center w-[80%] text-White ">
               The ability to use a smartphone, tablet, or computer to access
               your account means your files follow you everywhere.
             </p>
           </div>
           <div className=" flex flex-col font-Open gap-4 items-center">
             <img src={security} alt="" />
-            <h2 className=" text-xl text-White">Security you can trust</h2>
-            <p className=" text-center w-[75%] text-White ">
+            <h2 className=" text-2xl font-[700] text-White">
+              Security you can trust
+            </h2>
+            <p className=" text-[18px] text-center w-[75%] text-White ">
               2-factor authentication and user-controlled encryption are just a
               couple of the security features we allow to help secure your
               files.
@@ -57,16 +63,20 @@ function App() {
         <div className="flex flex-col gap-20 mt-20">
           <div className=" flex flex-col font-Open gap-4 items-center">
             <img src={collab} alt="" />
-            <h2 className=" text-xl text-White">Real-time collaboration</h2>
-            <p className=" text-center w-[75%] text-White ">
+            <h2 className=" text-2xl font-[700] text-White">
+              Real-time collaboration
+            </h2>
+            <p className=" text-[18px] text-center w-[82%] text-White ">
               Securely share files and folders with friends, family and
-              colleagues for live collaboration. No email attachments required
+              colleagues for live collaboration. No email attachments required.
             </p>
           </div>
           <div className=" flex flex-col font-Open gap-4 items-center">
             <img src={store} alt="" />
-            <h2 className=" text-xl text-White">Store any type of file</h2>
-            <p className=" text-center w-[75%] text-White ">
+            <h2 className=" text-2xl font-[700] text-White">
+              Store any type of file
+            </h2>
+            <p className=" text-[18px] text-center w-[85%] text-White ">
               Whether you're sharing holidays photos or work documents, Fylo has
               you covered allowing for all file types to be securely stored and
               shared.
@@ -76,7 +86,30 @@ function App() {
       </section>
       {/* end of section features */}
       {/* section works */}
-      <section></section>
+      <section className=" font-Open mt-[9rem] ">
+        <div>
+          <img src={productivity} className="w-[80%] mx-auto " alt="" />
+        </div>
+        <div className=" gap-4 mt-10 flex flex-col items-start px-8">
+          <h2 className=" font-[700] text-White text-2xl">
+            Stay productive, wherever you are
+          </h2>
+          <p className=" text-White font-[400] text-[18px] ">
+            Never let location be an issue when accessing your files. Fylo has
+            you covered for all of your file storage needs.
+          </p>
+          <p className="text-White font-[400] text-[18px]">
+            Securely share files and folders with friends, family and colleagues
+            for live collaboration. No email attachments required.
+          </p>
+          <div className="border-b flex gap-2 border-Cyan ">
+            <a className=" text-Cyan    " href="#">
+              See how Fylo works{" "}
+            </a>
+            <img src={arrow} className=" cursor-pointer w-5  " alt="" />{" "}
+          </div>
+        </div>
+      </section>
       {/* section works */}
     </main>
   );
